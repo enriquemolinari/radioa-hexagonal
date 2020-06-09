ada7f79db4b1d6b76e127faec148c60eb3b4366a
Enrique Pablo Molinari <enrique.molinari@gmail.com>
2020-06-09 17:37:59 -0300

    adding test for the architecture

---
+++ src/test/java/ar/cpfw/book/radio/TestLayers.java
@@ -0,0 +1,22 @@
+package ar.cpfw.book.radio;
+
+import static com.tngtech.archunit.library.Architectures.layeredArchitecture;
+
+import org.junit.Test;
+
+import com.tngtech.archunit.core.importer.ClassFileImporter;
+
+public class TestLayers {
+
+ @Test
+ public void testLayers() {
+  layeredArchitecture()
+    .layer("UI").definedBy("..ui..")
+    .layer("BusinessLogic").definedBy("..model..")
+    .layer("Persistence").definedBy("..persistence..")
+    .whereLayer("UI").mayNotBeAccessedByAnyLayer()
+    .whereLayer("BusinessLogic").mayOnlyBeAccessedByLayers("UI", "Persistence")
+    .whereLayer("Persistence").mayNotBeAccessedByAnyLayer()
+    .check(new ClassFileImporter().importPackages("ar.cpfw.book.radio"));
+ }
+}
